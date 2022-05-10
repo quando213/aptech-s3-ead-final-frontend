358d49290efa86cb31da16532322fb11ed3bfd46
Quan Do <ducquan1996@gmail.com>
2022-05-10 23:00:02 +0700

    Add project files.

---
+++ EadFinalFrontend/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using EadFinalFrontend.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EadFinalFrontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Service1Client service = new Service1Client();
+
+        public ActionResult Index()
+        {
+            var data = service.SearchEmployees(null);
+            return View(data);
+        }
+
+        public ActionResult SearchAjax()
+        {
+            var keyword = this.Request.QueryString["keyword"];
+            var result = service.SearchEmployees(keyword);
+            return PartialView("SearchAjax", result);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            var result = service.AddEmployee(employee);
+            if (!result)
+            {
+                return View("Error");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
